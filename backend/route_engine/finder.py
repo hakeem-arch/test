09505162505349282d75f166690ffa2dc416bb22
hakeem-arch <hakeem777362506@gmail.com>
2026-07-28 03:50:15 +0300

    Update finder.py

--- backend/route_engine/finder.py
+++ backend/route_engine/finder.py
@@ -20,8 +20,8 @@ import heapq
 from collections import deque
 from typing import Dict, List, Optional, Tuple
 
-from ..flight_graph import FlightGraph
-from ..flight_graph.geo import TRANSIT_PENALTY_MIN
+from backend.flight_graph import FlightGraph
+from backend.flight_graph.geo import TRANSIT_PENALTY_MIN
 
 from .models import RouteLeg, RoutePath
 
